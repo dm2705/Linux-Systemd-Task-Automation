ecac9b8c1259cc04d0bc63f6f589a2f569650e17
dm2705 <125991511+dm2705@users.noreply.github.com>
2023-11-06 19:44:37 -0700

    Add files via upload

---
+++ LinuxScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+HOMEPATH="path/to"
+PM2_Home="path/to/your/"
+
+#Get pid of task
+VAR=$(pm2 pid folder_of_file)
+
+#Task is launched if VAR is empty
+if [ -z "$VAR" ]||[ "$VAR" = 0 ]; then
+    #Navigate to the desired directory
+    cd " $PM2_HOME"
+    #Start Task
+    exec "PM2_HOME/executable_file.sh"
+else
+    #Task continues running is VAR is not empty
+    echo "Task is already running"
+fi
+
+#returns 0 if exit cases are correct 
