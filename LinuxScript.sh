3914a0842d71577bf73ce4a0677148946476d506
Digvijay Mukherjee <125991511+dm2705@users.noreply.github.com>
2023-11-18 14:58:17 -0700

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -2,15 +2,19 @@
 HOMEPATH="path/to"
 PM2_Home="path/to/folder/"
 
+#Check PM2 Daemon, start if not running VAR=$(pm2 ping)
 #Get pid of task
 VAR=$(pm2 pid folder_of_file)
+if grep -q "\[PM2\] Spawning PM2 daemon*" <<< "$VAR"; then
+VAR=0
+fi
 
 #Task is launched if VAR is empty
-if [ -z "$VAR" ]||[ "$VAR" = 0 ]; then
+if [ -z "$VAR" ]||[ "$VAR" = 0 ]||[ ]; then
     #Navigate to the desired directory
     cd " $PM2_HOME"
-    #Start Task
-    exec "PM2_HOME/executable_file.sh"
+    #Start Task on DISPLAY 0
+    DISPLAY=:0 exec "PM2_HOME/executable_file.sh"
 else
     #Task continues running is VAR is not empty
     echo "Task is already running"
